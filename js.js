1d697d2b35351cb759278a37f1c038ba8282a008
Shirley Zhang <shirley.zhang@hotmail.com.au>
2018-10-01 17:53:44 +1000

    Added some visual stuff

--- js.js
+++ js.js
@@ -11,12 +11,14 @@ function genPlant() {
   plant.eating = roll(edible);
   if (plant.eating == "edible") {
     plant.traits.push({
+      traitType: "Cooking",
       plantPart: roll(plantPart),
-      flavor: roll(plantFlavour),
+      flavour: roll(plantFlavour),
       prep: roll(plantPreparation)
     });
   } else if (plant.eating == "poisonous") {
     plant.traits.push({
+      traitType: "Poisons",
       plantPart: roll(plantPart),
       severity: roll(poisonSeverity),
       method: roll(poisonMethod)
@@ -32,30 +34,40 @@ function genPlant() {
 
   if (plant.utility == "dyes") {
     plant.traits.push({
+      traitType: "Dyes",
       plantPart: roll(plantPart),
       dyeColour: roll(dyeColours)
     })
   } else if (plant.utility == "cosmetic") {
     plant.traits.push({
+      traitType: "Cosmetic",
       plantPart: roll(plantPart),
-      cosmeticForm: roll(cosmeticUses)
+      cosmeticForm: roll(cosmeticForm),
+      cosmeticUse: roll(cosmeticUses)
     })
   } else if (plant.utility == "medicinal") {
     plant.traits.push({
+      traitType: "Medicine",
       plantPart: roll(plantPart),
       medicinalUse: roll(medicinalUses)
     })
   } else if (plant.utility == "religious") {
     plant.traits.push({
+      traitType: "Religion",
+      plantPart: roll(plantPart),
       religiousUse: roll(reiligiousUses)
     })
   } else if (plant.utility == "magical") {
     plant.traits.push({
+      traitType: "Magic",
+      plantPart: roll(plantPart),
       religiousUse: roll(reiligiousUses)
     })
   } else if (plant.utility == "textiles/weaving") {
     plant.traits.push({
-      religiousUse: roll(reiligiousUses)
+      traitType: "Textiles",
+      plantPart: roll(plantPart),
+      textileGrade: roll(textileGrade)
     })
   }
 
@@ -63,6 +75,7 @@ function genPlant() {
   // Roll ornamental y/n
   // display plant
   console.log(plant);
+  vm.plants.unshift(plant);
 }
 
 function roll (givenArray) {
@@ -84,7 +97,8 @@ var nonPoisonUtility = ["none", "medicinal", "cooking", "dyes", "weaving/textile
 var poisonUtility = ["none", "medicinal", "cosmetic", "religious", "assassination/murder", "hunting/trapping", "battle", "magical"];
 
 var dyeColours = ["light blue", "deep blue", "teal", "light green", "deep green", "sap green", "yellow-green", "yellow", "red", "orange", "pink", "red-orange"];
-var cosmeticUses = ["powder", "paste", "toner", "tonic", "cream"];
+var cosmeticForm = ["powder", "paste", "toner", "tonic", "cream"];
+var cosmeticUses = ["moisturising", "colouring", "shimmer", "drawing/lining", "perfume", "hair/beard powder"];
 var medicinalUses = ["healing", "anaesthetic", "anti-nausea", "anti-fungal", "anti-bacterial", "bone/marrow repair", "replenishes blood", "good for skin/healing scars"];
 var reiligiousUses = ["cleansing", "ritual", "offering", "symbolic", "incense", "experiencing"];
 var magicalUses = ["banishing", "blessing", "elemental", "illusionary", "summoning"];
---
+++ vueSetup.js
@@ -0,0 +1,9 @@
+console.log("Hello comoponent")
+
+var vm = new Vue({
+  data: {
+    plants: []
+  }
+}).$mount('#wrapper')
+
+
